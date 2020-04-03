1c2f805041b97ec1a503ef31a6bdbe1e21b55ef7
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2020-04-04 01:42:14 +0200

    WIP alert test

--- alert_test.go
+++ alert_test.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,50 +10,79 @@ import (
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
-// &{[{0 [{requests_total map[] [time mean] [[2020-04-03T14:21:23.840000791Z 99.9]] false}] [] }] }
+func TestAlert(t *testing.T) {
 
-func TestAlert_Alert(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		data := client.Response{
-			Results: []client.Result{{
-				StatementId: 0,
-				Series:      nil,
-				Messages:    nil,
-				Err:         "",
-			}},
-			Err: "",
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("X-Influxdb-Version", "1.3.1")
-		w.WriteHeader(http.StatusOK)
-		enc := json.NewEncoder(w)
-		_ = enc.Encode(data)
-	}))
-	defer ts.Close()
-
-	config := client.HTTPConfig{Addr: ts.URL}
+	config := client.HTTPConfig{}
 	c, _ := client.NewHTTPClient(config)
 	defer c.Close()
-
-	query := client.Query{}
-	resp, err := c.Query(query)
-	if err != nil {
-		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
-	}
-
-	if got, exp := len(resp.Results), 1; got != exp {
-		t.Fatalf("unexpected number of results.  expected %v, actual %v", exp, got)
-	}
 
 	var alerter Alerter
 	alerter = &CommonLogAlert{client: c, filename: "test"}
 	alertCh := make(chan string)
 	go alerter.Alert(10, alertCh)
 
-	select {
-	case msg := <-alertCh:
-		fmt.Println("Alert", msg)
-	case <-time.After(3 * time.Second):
-		fmt.Println("timeout 2")
+	cases := []struct {
+		name     string
+		response []byte
+		message  string
+	}{
+		{
+			name: "generate alert",
+			response: []byte(`
+			{"results":[
+				{"statement_id":0,
+				"series":
+					[{
+						"requests_total":["time","mean"],
+						"values":[["2020-04-03T14:21:23.840000791Z",99.9]]
+					}]
+				}
+			]}	
+			`),
+			message: "Alert High traffic generated an alert - hits = 99.9, triggered at 2020-04-04 01:10:43.752866 +0200 CEST m=+0.003371168",
+		},
+		{
+			name: "recovery alert",
+			response: []byte(`
+			{"results":[
+				{"statement_id":0,
+				"series":
+					[{
+						"requests_total":["time","mean"],
+						"values":[["2020-04-03T14:21:23.840000791Z",99.9]]
+					}]
+				}
+			]}	
+			`),
+			message: "Alert High traffic generated an alert - hits = 99.9, triggered at 2020-04-04 01:10:43.752866 +0200 CEST m=+0.003371168",
+		},
+	}
+	for _, tc := range cases {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("X-Influxdb-Version", "1.3.1")
+			w.WriteHeader(http.StatusOK)
+			w.Write(tc.response)
+		}))
+		defer ts.Close()
+
+		query := client.Query{}
+		resp, err := c.Query(query)
+		if err != nil {
+			t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
+		}
+
+		if got, exp := len(resp.Results), 1; got != exp {
+			t.Fatalf("unexpected number of results.  expected %v, actual %v", exp, got)
+		}
+
+		select {
+		case msg := <-alertCh:
+			if msg != tc.message {
+				t.Logf("Alert expected: %s\n Alert received: %s\n", tc.message, msg)
+			}
+		case <-time.After(5 * time.Second):
+			t.Fatalf("Timed out waiting for msg: %s", tc.message)
+		}
 	}
 }
