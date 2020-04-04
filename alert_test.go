a70deb383b09e30dc8e289b5b598429acd408540
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2020-04-04 10:30:38 +0200

    Alert test

--- alert_test.go
+++ alert_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -10,16 +11,31 @@ import (
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
+// TestAlert tests the alert logic:
+// 1. Create an alert
+// 2. Check that it doesn't retrigger the alert
+// 3. Recover the alert
+// 4. Check that it doesn't retrigger the recovery
+// The default alert poll period is 10 sec so the test has to wait it times out
+// and takes more than 35 seconds to be executed
 func TestAlert(t *testing.T) {
+	var response []byte
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("X-Influxdb-Version", "1.3.1")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	}))
+	defer ts.Close()
 
-	config := client.HTTPConfig{}
+	config := client.HTTPConfig{Addr: ts.URL}
 	c, _ := client.NewHTTPClient(config)
 	defer c.Close()
 
-	var alerter Alerter
-	alerter = &CommonLogAlert{client: c, filename: "test"}
+	alerter := NewAlert(c, "test")
 	alertCh := make(chan string)
 	go alerter.Alert(10, alertCh)
+	defer close(alertCh)
 
 	cases := []struct {
 		name     string
@@ -39,7 +55,22 @@ func TestAlert(t *testing.T) {
 				}
 			]}	
 			`),
-			message: "Alert High traffic generated an alert - hits = 99.9, triggered at 2020-04-04 01:10:43.752866 +0200 CEST m=+0.003371168",
+			message: "Alert High traffic generated an alert - hits = 99.9, triggered at",
+		},
+		{
+			name: "alert high, do nothing",
+			response: []byte(`
+			{"results":[
+				{"statement_id":0,
+				"series":
+					[{
+						"requests_total":["time","mean"],
+						"values":[["2020-04-03T14:21:23.840000791Z",19.9]]
+					}]
+				}
+			]}	
+			`),
+			message: "",
 		},
 		{
 			name: "recovery alert",
@@ -49,23 +80,32 @@ func TestAlert(t *testing.T) {
 				"series":
 					[{
 						"requests_total":["time","mean"],
-						"values":[["2020-04-03T14:21:23.840000791Z",99.9]]
+						"values":[["2020-04-03T14:21:23.840000791Z",5.1]]
 					}]
 				}
 			]}	
 			`),
-			message: "Alert High traffic generated an alert - hits = 99.9, triggered at 2020-04-04 01:10:43.752866 +0200 CEST m=+0.003371168",
+			message: "High traffic alert recovered - hits = 5.1, triggered at",
+		},
+		{
+			name: "no alert, do nothing",
+			response: []byte(`
+			{"results":[
+				{"statement_id":0,
+				"series":
+					[{
+						"requests_total":["time","mean"],
+						"values":[["2020-04-03T14:21:23.840000791Z",3.1]]
+					}]
+				}
+			]}	
+			`),
+			message: "",
 		},
 	}
+	// Has to be serial, order of execution matters
 	for _, tc := range cases {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("X-Influxdb-Version", "1.3.1")
-			w.WriteHeader(http.StatusOK)
-			w.Write(tc.response)
-		}))
-		defer ts.Close()
-
+		response = tc.response
 		query := client.Query{}
 		resp, err := c.Query(query)
 		if err != nil {
@@ -78,11 +118,13 @@ func TestAlert(t *testing.T) {
 
 		select {
 		case msg := <-alertCh:
-			if msg != tc.message {
+			if strings.Contains(msg, tc.message) {
 				t.Logf("Alert expected: %s\n Alert received: %s\n", tc.message, msg)
 			}
-		case <-time.After(5 * time.Second):
-			t.Fatalf("Timed out waiting for msg: %s", tc.message)
+		case <-time.After(15 * time.Second):
+			if len(tc.message) > 0 {
+				t.Fatalf("Timed out waiting for msg: %s", tc.message)
+			}
 		}
 	}
 }
