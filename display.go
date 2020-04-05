b781780ecdcd804a4abd61a3d12274c275dd27f9
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2020-04-05 14:26:48 +0200

    Display results

--- display.go
+++ display.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
@@ -38,12 +39,13 @@ func (c CommonLogDisplay) Display(eventCh <-chan string) error {
 	}
 	defer ui.Close()
 
-	ev := ui.PollEvents()
+	uiEvents := ui.PollEvents()
 
 	// Paragraph
 	p := widgets.NewParagraph()
-	p.Text = "HTTP Log Analyzer"
-	p.SetRect(0, 0, 100, 5)
+	p.Title = "HTTP Log Analyzer"
+	p.Text = "\n\n     PRESS q TO QUIT"
+	p.SetRect(0, 0, 80, 5)
 
 	// Create list of alerts
 	alerts := widgets.NewList()
@@ -51,7 +53,7 @@ func (c CommonLogDisplay) Display(eventCh <-chan string) error {
 
 	alerts.TextStyle = ui.NewStyle(ui.ColorYellow)
 	alerts.WrapText = false
-	alerts.SetRect(0, 5, 100, 15)
+	alerts.SetRect(0, 5, 80, 15)
 
 	// Create list of top sections
 	l := widgets.NewList()
@@ -59,39 +61,44 @@ func (c CommonLogDisplay) Display(eventCh <-chan string) error {
 
 	l.TextStyle = ui.NewStyle(ui.ColorYellow)
 	l.WrapText = false
-	l.SetRect(0, 15, 20, 25)
+	l.SetRect(0, 15, 30, 25)
 
-	// Create SparkLine with the bytes received
-	data := []float64{4, 2, 1, 6, 3, 9, 1, 4, 2, 15, 14, 9, 8, 6, 10, 13, 15, 12, 10, 5, 3, 6, 1, 7, 10, 10, 14, 13, 6}
+	// Create Plot with the bytes received
+	p1 := widgets.NewPlot()
+	p1.Title = "KiloBytes per second"
+	p1.Marker = widgets.MarkerDot
+	p1.Data = make([][]float64, 2)
+	p1.SetRect(30, 15, 55, 25)
+	p1.AxesColor = ui.ColorWhite
+	p1.LineColors[0] = ui.ColorCyan
+	p1.PlotType = widgets.ScatterPlot
 
-	sl0 := widgets.NewSparkline()
-	sl0.Data = data[3:]
-	sl0.LineColor = ui.ColorGreen
+	// Create Plot with the requests received
+	p2 := widgets.NewPlot()
+	p2.Title = "Requests per second"
+	p2.Marker = widgets.MarkerDot
+	p2.Data = make([][]float64, 2)
+	p2.SetRect(55, 15, 80, 25)
+	p2.AxesColor = ui.ColorWhite
+	p2.LineColors[0] = ui.ColorCyan
+	p2.PlotType = widgets.ScatterPlot
 
-	// single
-	slg0 := widgets.NewSparklineGroup(sl0)
-	slg0.Title = "Bytes"
-	slg0.SetRect(20, 15, 60, 25)
-
-	// Create SparkLine with the number of requests
-
-	sl1 := widgets.NewSparkline()
-	sl1.Data = data[3:]
-	sl1.LineColor = ui.ColorGreen
-
-	// single
-	slg1 := widgets.NewSparklineGroup(sl1)
-	slg1.Title = "Requests"
-	slg1.SetRect(60, 15, 100, 25)
+	// Draw
+	draw := func() {
+		// update dashboard every 10 second
+		l.Rows = c.getTopSection()
+		p1.Data[0] = c.getBytesSecond()
+		p2.Data[0] = c.getRequestsSecond()
+		ui.Render(p, l, alerts, p1, p2)
+	}
+	draw()
 
 	for {
 		select {
-		case e := <-ev:
-			switch e.Type {
-			case ui.KeyboardEvent:
-				// quit on any keyboard event
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
 				return nil
-			case ui.ResizeEvent:
 			}
 		case msg := <-eventCh:
 			alerts.Rows = append(alerts.Rows, msg)
@@ -99,58 +106,69 @@ func (c CommonLogDisplay) Display(eventCh <-chan string) error {
 			if len(alerts.Rows) > 10 {
 				alerts.Rows = alerts.Rows[1:]
 			}
-			ui.Render(p, l, alerts, slg0, slg1)
+			draw()
 
 		case <-time.After(10 * time.Second):
-			// update dashboard every 10 second
-			l.Rows = c.getTopSection()
-			sl0.Data = c.getBytesSecond()
-			sl1.Data = c.getRequestsSecond()
-			ui.Render(p, l, alerts, slg0, slg1)
+			draw()
 		}
 	}
 
 	return nil
 }
 
+// TODO: getTopSection, getBytesSecond and getBytesSecond can reduce a lot of duplicate code
 func (c CommonLogDisplay) getTopSection() []string {
-	query := fmt.Sprintf("SELECT top(value,5),section FROM requests_section_count WHERE (file = '%s' AND time > now() - 19s)", c.filename)
+	var result []string
+	// Statsd aggregates by 10s, otherwise we should do a subquery to aggregate first all occurrences during the period
+	query := fmt.Sprintf("SELECT top(value,5),section FROM requests_section_count WHERE (file = '%s' AND time > now() - 15s)", c.filename)
 	q := client.NewQuery(query, "statsd", "")
 	response, err := c.client.Query(q)
-	if err != nil {
-		return nil
-	}
 	if err == nil && response.Error() == nil && len(response.Results[0].Series) > 0 {
-		// _ = response.Results[0].Series[0]
-		return nil
+		for i := range response.Results[0].Series[0].Values {
+			// t := response.Results[0].Series[0].Values[i][0]
+			top := response.Results[0].Series[0].Values[i][1]
+			section := response.Results[0].Series[0].Values[i][2]
+			// termui doesn't support \t tabs :/
+			result = append(result, fmt.Sprintf("[%v] %v  Hits: %v", i+1, section, top))
+		}
 	}
-	return []string{"one", "two", "three"}
+	return result
 }
 
 func (c CommonLogDisplay) getBytesSecond() []float64 {
-	query := fmt.Sprintf("SELECT last(value) FROM requests_bytes_count WHERE (file = '%s' AND time > now() - 2m) GROUP BY time(10s)", c.filename)
+	var result []float64
+	// Get last 2 minutes bytes time series
+	query := fmt.Sprintf("SELECT last(value) FROM requests_bytes_count WHERE (file = '%s' AND time > now() - 2m) GROUP BY time(10s) FILL(0)", c.filename)
 	q := client.NewQuery(query, "statsd", "")
 	response, err := c.client.Query(q)
-	if err != nil {
-		return nil
-	}
 	if err == nil && response.Error() == nil && len(response.Results[0].Series) > 0 {
-		// _ = response.Results[0].Series[0]
-		return nil
+		for i := range response.Results[0].Series[0].Values {
+			// t := response.Results[0].Series[0].Values[i][0]
+			bytes, _ := response.Results[0].Series[0].Values[i][1].(json.Number).Float64()
+			// TODO: Aggregated period is a constant (10s), make it a variable
+			kbps := bytes / (1024 * 10)
+			result = append(result, kbps)
+		}
+		// TODO: review influxdb query to not return always last result as 0
+		result = result[:len(result)-2]
 	}
-	return []float64{4, 2, 1, 6, 3, 9, 1, 4, 2, 15, 14, 9, 8, 6, 10, 13, 15, 12, 10, 5, 3, 6, 1, 7, 10, 10, 14, 13, 6}
+	return result
 }
 
 func (c CommonLogDisplay) getRequestsSecond() []float64 {
-	query := fmt.Sprintf("SELECT last(value) FROM requests_total WHERE (file = '%s' AND time > now() - 2m) GROUP BY time(10s)", c.filename)
+	var result []float64
+	// Get last 2 mins requests time series
+	query := fmt.Sprintf("SELECT last(value) FROM requests_total WHERE (file = '%s' AND time > now() - 2m) GROUP BY time(10s) FILL(0)", c.filename)
 	q := client.NewQuery(query, "statsd", "")
 	response, err := c.client.Query(q)
-	if err != nil {
-		return nil
-	}
 	if err == nil && response.Error() == nil && len(response.Results[0].Series) > 0 {
-		// _ = response.Results[0].Series[0]
-		return nil
+		for i := range response.Results[0].Series[0].Values {
+			// t := response.Results[0].Series[0].Values[i][0]
+			requests, _ := response.Results[0].Series[0].Values[i][1].(json.Number).Float64()
+			// TODO: Aggregated period is a constant (10s), make it a variable
+			rps := requests / 10
+			result = append(result, rps)
+		}
 	}
-	return []float64{4, 2, 1, 6, 3, 9, 1, 4, 2, 15, 14, 9, 8, 6, 10, 13, 15, 12, 10, 5, 3, 6, 1, 7, 10, 10, 14, 13, 6}
+	return result
 }
